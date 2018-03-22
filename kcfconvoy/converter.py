5b8197251637cbfa111b51cd595dacc208f2f19e
sato-masayukidesu <maatanbeta@gmail.com>
2018-03-22 11:06:26 +0900

    bug fix

--- kcfconvoy/converter.py
+++ kcfconvoy/converter.py
@@ -10,25 +10,25 @@ def similarity(kcf_vec_1, kcf_vec_2, n_nodes=list(range(99)),
     kegg_atom_levels = set([kegg_atom_levels[level] for level in levels])
     l_count_1 = []
     l_count_2 = []
-    for ele, label in kcf_vec_1.kegg_atom_label.items():
+    for ele, label in kcf_vec_1.kcf_vec.items():
         if not label["n_nodes"] in n_nodes:
             continue
         if not label["ele_level"] in kegg_atom_levels:
             continue
-        if ele in kcf_vec_2.kegg_atom_label.keys():
-            l_count_1.append(kcf_vec_1.kegg_atom_label[ele]["count"])
-            l_count_2.append(kcf_vec_2.kegg_atom_label[ele]["count"])
+        if ele in kcf_vec_2.kcf_vec.keys():
+            l_count_1.append(kcf_vec_1.kcf_vec[ele]["count"])
+            l_count_2.append(kcf_vec_2.kcf_vec[ele]["count"])
         else:
-            l_count_1.append(kcf_vec_1.kegg_atom_label[ele]["count"])
+            l_count_1.append(kcf_vec_1.kcf_vec[ele]["count"])
             l_count_2.append(0)
-    for ele, label in kcf_vec_2.kegg_atom_label.items():
+    for ele, label in kcf_vec_2.kcf_vec.items():
         if not label["n_nodes"] in n_nodes:
             continue
         if not label["ele_level"] in kegg_atom_levels:
             continue
-        if ele not in kcf_vec_1.kegg_atom_label.keys():
+        if ele not in kcf_vec_1.kcf_vec.keys():
             l_count_1.append(0)
-            l_count_2.append(kcf_vec_2.kegg_atom_label[ele]["count"])
+            l_count_2.append(kcf_vec_2.kcf_vec[ele]["count"])
 
     np_count_1 = np.array(l_count_1)
     np_count_2 = np.array(l_count_2)
