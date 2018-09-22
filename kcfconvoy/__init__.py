6f33fdee19b066f141732c57cae2289b3af9d793
suecharo <suehiro619@gmail.com>
2018-09-22 11:06:07 +0900

    Update setup.py

--- kcfconvoy/__init__.py
+++ kcfconvoy/__init__.py
@@ -1,5 +1,1 @@
-#!/usr/bin/env python
 # coding: utf-8
-__author__ = 'maskot1977'
-__version__ = '0.0.3'
-__license__ = 'MIT'
--- setup.py
+++ setup.py
@@ -1,23 +1,40 @@
-#!/usr/bin/env python
 # coding: utf-8
 from setuptools import setup, find_packages
-from kcfconvoy import __author__, __version__, __license__
 
-setup(
-        name             = 'kcfconvoy',
-        version          = __version__,
-        url              = 'https://github.com/KCF-Convoy',
-        author           = __author__,
-        author_email     = 'maskot1977@gmail.com',
-        description      = 'KCF convoy: requires rdkit installed in advance',
-        license          = __license__,
-        keywords         = 'sample pip github python',
-        packages         = find_packages(),
-        install_requires = ["matplotlib",
-                            "networkx",
-                            "numpy",
-                            "IPython",
-                            "pandas",
-                            "pillow"
-                            ],
-        )
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+
+def main():
+    setup(
+        name="kcfconvoy",
+        version="0.0.4",
+        url="https://github.com/KCF-Convoy",
+        author="maskot1977",
+        author_email="maskot1977@gmail.com",
+        description="KCF convoy: requires rdkit installed in advance",
+        license="MIT",
+        keywords="bio-Informatics kcf kcfconvoy smiles",
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: POSIX :: Linux",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3 :: Only",
+            "Topic :: Scientific/Engineering",
+            "Topic :: Scientific/Engineering :: Bio-Informatics",
+            "Topic :: Scientific/Engineering :: Chemistry",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+        packages=find_packages(),
+        install_requires=requirements
+    )
+
+
+if __name__ == "__main__":
+    main()
